d3f9f2714ad3ffd6a0ee5f97525293d476e786e8
varshini-k-02 <varshinikarthik002@gmail.com>
2021-06-10 19:40:24 +0530

    first commit

---
+++ sparksIn/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sparksIn project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sparksIn.settings')
+
+application = get_asgi_application()
