f79883ec60b3eda7ca16120fd9ebf6f31dc92e22
varshini-k-02 <varshinikarthik002@gmail.com>
2021-06-13 22:33:05 +0530

    payment initial

--- secondchance/views.py
+++ secondchance/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import razorpay
+from django.views.decorations.csrf import csrf_exempt
 
-# Create your views here.
+def home(request):
+        order_amount = 50000
+        order_currency = 'INR'
+        order_receipt = 'order_rcptid_11'
+        notes = {'Shipping address': 'Bommanahalli, Bangalore'}   
+        # OPTIONALclient.order.create(amount=order_amount, currency=order_currency, receipt=order_receipt, notes=notes)    
+        return render(request,'index.html')
+
+@csrf_exempt
+def success(request):
+    return render(request, "success.html")
